0a1af9ef3201da29eb80902e3863b5fe9dc12e5d
pablo <paulandrewjeffrey@gmail.com>
2021-03-07 19:09:42 -0800

    CSS changes depending on time

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const nine = document.getElementById('9am')
 const ten = document.getElementById('10am')
 const eleven = document.getElementById('11am')
 const twelve = document.getElementById('12pm')
-const one = document.getElementById('1pm')
-const two = document.getElementById('2pm')
-const three = document.getElementById('3pm')
-const four = document.getElementById('4pm')
+const thirteen = document.getElementById('1pm')
+const fourteen = document.getElementById('2pm')
+const fifteen = document.getElementById('3pm')
+const sixteen = document.getElementById('4pm')
 
 function onLoad() {
     const time = document.getElementById('currentDay')
     time.innerText = moment().format("MMM Do YY")
+    assignClass()
     if (localStorage.schedule) {
         schedule = JSON.parse(localStorage.getItem('schedule'))
         document.getElementById('9amtext').innerText = schedule[0].entry
@@ -87,7 +88,47 @@ nine.addEventListener('click', update)
 ten.addEventListener('click', update)
 eleven.addEventListener('click', update)
 twelve.addEventListener('click', update)
-one.addEventListener('click', update)
-two.addEventListener('click', update)
-three.addEventListener('click', update)
-four.addEventListener('click', update)
+thirteen.addEventListener('click', update)
+fourteen.addEventListener('click', update)
+fifteen.addEventListener('click', update)
+sixteen.addEventListener('click', update)
+
+function assignClass() {
+    var hours = [
+        {
+            number: 9, element: document.getElementById('9amtext'),
+        },
+        {
+            number: 10, element: document.getElementById('10amtext'),
+        },
+        {
+            number: 11, element: document.getElementById('11amtext'),
+        },
+        {
+            number: 12, element: document.getElementById('12pmtext'),
+        },
+        {
+            number: 13, element: document.getElementById('1pmtext'),
+        },
+        {
+            number: 14, element: document.getElementById('2pmtext'),
+        },
+        {
+            number: 15, element: document.getElementById('3pmtext'),
+        },
+        {
+            number: 16, element: document.getElementById('4pmtext')
+        }
+    ]
+ 
+    for (i = 0; i < hours.length; i++) {
+        console.log(hours[i].number)
+        if (hours[i].number < moment().format('HH')) {
+            hours[i].element.classList.add('past')
+        } else if (hours[i].number == moment().format('HH')) {
+            hours[i].element.classList.add('present')
+        } else if (hours[i].number > moment().format('HH')) {
+            hours[i].element.classList.add('future')
+        }
+    }
+}
